6534430c4a0b616309b8a4785c1efc2a659f3515
Yuvraj Armaja Pun <yuvrajpun544@gmail.com>
2025-07-07 11:57:57 +0545

    starting

---
+++ src/Book.java
@@ -0,0 +1,21 @@
+public class Book {
+    int bookNumber;
+    String name;
+    int quantity;
+    String author;
+
+    public Book(int bookNumber, String name, int quantity, String author){
+        this.bookNumber = bookNumber;
+        this.name = name;
+        this.quantity = quantity;
+        this.author = author;
+    }
+
+    public void increaseQuantity(){
+
+    }
+
+    public void decreaseQuantity(){
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,19 @@
+public class User {
+    String name;
+    String username;
+    String contact;
+
+    public User(String name, String username, String contact){
+        this.name = name;
+        this.username = username;
+        this.contact = contact;
+    }
+
+    public void borrowBook(){
+
+    }
+
+    public void returnBook(){
+
+    }
+}
